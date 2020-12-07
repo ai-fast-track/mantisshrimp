e8c8b198dbb51936d5fa6877ea4db51753f3c518
ai-fast-track <farid@itecpedia.com>
2020-12-06 22:54:50 -0500

    added get_dataloaders()

--- icevision/models/utils.py
+++ icevision/models/utils.py
@@ -11,6 +11,7 @@ from icevision.imports import *
 from icevision.utils import *
 from icevision.core import *
 from icevision.parsers import *
+from icevision.data.dataset import Dataset
 
 
 BN_TYPES = (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)
@@ -80,3 +81,67 @@ def _predict_dl(
         all_preds.extend(preds)
 
     return all_samples, all_preds
+
+
+def get_dataloaders(
+    model_type,
+    records_list: List[List[dict]],
+    tfms_list: List[Transform],
+    size,
+    presize,
+    batch_tfms=None,
+    batch_size=16,
+    num_workers=4,
+    **dataloader_kwargs,
+):
+    """
+    Creates and returns datasets and dataloaders:
+
+    # Arguments
+        model_type: can be one of these values: faster_rcnn, retinanet, mask_rcnn, efficientdet
+        records: A list of records ->  [train_records, valid_records].
+            Both train_records, valid_records are of type List[dict]
+        tfms: List of Transforms to be applied to each dataset: [train_tfms, valid_tfms].
+            Both train_tfms and valid_tfms are of type Transform
+        size: The final size of the image.
+        presizing: Rescale the image before applying other transfroms.
+        batch_size: batch size.
+        num_workers: number of workers.
+
+    # Return
+        - datasets: List containing train_ds and valid_ds -> [train_ds, valid_ds]
+        - dataloaders: List containing train_dl and valid_dl -> [train_dl, valid_dl]
+    """
+
+    ds = []
+    dls = []
+
+    # Datasets
+    train_ds = Dataset(records_list[0], tfms_list[0])
+    valid_ds = Dataset(records_list[1], tfms_list[1])
+
+    # Dataloaders
+    train_dl = model_type.train_dl(
+        train_ds,
+        batch_tfms=batch_tfms,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=True,
+        **dataloader_kwargs,
+    )
+    valid_dl = model_type.valid_dl(
+        valid_ds,
+        batch_tfms=batch_tfms,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=False,
+        **dataloader_kwargs,
+    )
+
+    ds.append(train_ds)
+    ds.append(valid_ds)
+
+    dls.append(train_dl)
+    dls.append(valid_dl)
+
+    return dls
